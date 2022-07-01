1de890c11f2082468b3507404d4ec67b716f5e5b
Subhan Khan <subhank1997@gmail.com>
2022-07-01 13:19:00 -0400

    commit tree b7c4c9c5243af6746fa77112d574d51c5479eab6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind(side1,side2,side3)
+    if side1 == side2 && side3
+      :equilateral 
+    elsif side1 == side2 || side2 == side3 || side1 ==side3
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
 end
